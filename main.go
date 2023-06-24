8dea50db8213ed4c96a8eb972b3f2f07b1383b0b
rhythmjayee <jayeerythm8@gmail.com>
2023-06-24 22:42:06 +0530

    /add api

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+var todos map[int] string
+var counter int
+
+type Todo struct {
+	Text   string `json:"text" binding:"required"`
+}
 
 func testAPI(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -8,8 +18,35 @@ func testAPI(c *gin.Context) {
 	})
 }
 
+func addTodo(c *gin.Context) {
+	var todoJson Todo
+	if err := c.ShouldBindJSON(&todoJson); err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	fmt.Println(todoJson)
+	if todoJson.Text != "" {
+		var todo = todoJson.Text
+		counter++
+		todos[counter] = todo
+		c.JSON(200, gin.H{
+			"id": counter,
+			"message": "Todo : {"+ todo +"} has been saved.",
+		})
+		return
+	}
+	c.JSON(400, gin.H{
+		"error": "Something went wrong",
+	}) 
+}
+
 func main() {
 	route := gin.Default()
+	todos = make(map[int] string)
+	counter = 0
 	route.GET("/test", testAPI)
+	route.POST("/add", addTodo)
 	route.Run() // listen and serve on 0.0.0.0:8080
 }

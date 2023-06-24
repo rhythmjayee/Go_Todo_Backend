27f6302d87d9f01eb91b836efc62bb69c98edafa
rhythmjayee <jayeerythm8@gmail.com>
2023-06-24 23:16:55 +0530

    /todos api

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,12 +10,42 @@ var todos map[int] string
 var counter int
 
 type Todo struct {
+	Id int
 	Text   string `json:"text" binding:"required"`
+}
+type TodoList struct {
+	Todos   []Todo 
 }
 
 func testAPI(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "API is working",
+	})
+}
+
+func getTodos(c *gin.Context) {
+	len := len(todos)
+	fmt.Println(len)
+	list := TodoList{}
+	(list).Todos = make([]Todo, len)
+	count := 0
+	for k,v := range todos {
+		(list).Todos[count] = Todo{
+			Id: k,
+			Text: v,
+		}
+		count = count + 1
+	}
+	res, err := json.Marshal(list)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	fmt.Println(res)
+	c.JSON(200, gin.H{
+		"Todos": list.Todos,
 	})
 }
 
@@ -47,6 +78,7 @@ func main() {
 	todos = make(map[int] string)
 	counter = 0
 	route.GET("/test", testAPI)
+	route.GET("/todos", getTodos)
 	route.POST("/add", addTodo)
 	route.Run() // listen and serve on 0.0.0.0:8080
 }
